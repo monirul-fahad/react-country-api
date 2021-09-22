8fb392a7906c38cf54aeb97b1abccf1bf4712c43
Monirul Fahad <immonirulfahad@gmail.com>
2021-09-22 21:53:28 +0600

    first commit

---
+++ src/components/Country/Country.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Country.css";
+
+const Country = (props) => {
+  const { name, flag, capital, population, region } = props.country;
+  return (
+    <div className="country">
+      <h4>Name: {name}</h4>
+      <img src={flag} alt="" />
+      <p>
+        <small>Region: {region}</small>
+      </p>
+      <p>
+        Capital is: {capital}. Population: {population}
+      </p>
+    </div>
+  );
+};
+
+export default Country;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Foote.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        <small>Copyright Protected</small>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
